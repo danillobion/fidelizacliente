8c8213a9ebcc30c886e2c3416f75f0761afb5ca8
Danillo Bion <danillobion@gmail.com>
2023-06-25 10:52:30 -0300

    criptografando senha do usuario

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,13 +1,16 @@
 const Usuario = require('../models/Usuario');
 const Permissao = require('../models/Permissao');
+const bcrypt = require("bcrypt");
+
 module.exports={
 	async store(req,res){
 		const {nome,email,senha} = req.body;
+		const hashedSenha = await bcrypt.hash(senha+"|"+email, 10);
 		const permissao = await Permissao.findByPk(1);
 		if(!permissao){
 			return res.status(400).json({error: "Permissão não encontrada"});
 		}
-		const usuario = await Usuario.create({nome,email,senha,permissoes:permissao.nome});
+		const usuario = await Usuario.create({nome,email,senha:hashedSenha,permissoes:permissao.nome});
 		return res.json(usuario);
 	},
 	async findAll(req,res){
